3ec8b23edf230f77730276460fde10f98772090d
Henrik Sandklef <hesa@sandklef.com>
2016-06-23 12:41:20 +0200

    while #6 solution

---
+++ while/6/While.java
@@ -0,0 +1,18 @@
+
+public class While {
+
+    public static void main(String[] args) {
+
+        /*        System.out.println("Hi there..");
+                  System.out.println("Hi there..");
+                  System.out.println("Hi there..");
+        */
+        int i=0;
+        while (i<4) {
+            System.out.println("Hi there..");
+            i++;
+        }
+    }
+
+
+}
