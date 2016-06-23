88b7b271f927a694364a0446875e7756b6391693
Henrik Sandklef <hesa@sandklef.com>
2016-06-23 13:09:52 +0200

    for #5 solution

---
+++ for/5/For.java
@@ -0,0 +1,19 @@
+
+public class For {
+
+    public static void main(String[] args) {
+
+        /*
+        int counter=0;
+        while (counter<3) {
+            System.out.println("Hello");
+            counter++;
+        }
+        */
+        for (int counter=0;counter<3;counter++) {
+            System.out.println("Hello");
+        }
+    }
+
+
+}
